c92d953695bf21ef0ad631580090a2e994604dcd
Ranzer-Dev <rodolfo.antunes9@gmail.com>
2025-09-16 21:29:13 -0300

    feat: testando rotas e aumentando a complexidade

---
+++ app.js
@@ -0,0 +1,13 @@
+//importando os módulos do framework
+const express = require('express')
+const app = express()
+
+//importando o módulo de rotas que criamos
+const loginRoute = require('./routes/LoginRoute')
+
+//usado para adicionar router ao caminho de gerenciamento de middleware
+app.use(loginRoute)
+
+app.listen(3000, function() {
+    console.log("APP rodando na porta 3000")
+})
--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-router.get("/login", (req, res) => {
-    res.send('<h1>Login')
-})
+const loginController = require("../controller/LoginController")
 
+router.get("/login", loginController.getLogin)
 module.exports = router
